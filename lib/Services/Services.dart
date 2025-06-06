677906ce416512b450a42381da750759fa154c87
LENOVO <khatritirth418@gmail.com>
2025-06-06 21:41:58 +0530

    initial commit

---
+++ lib/Services/Services.dart
@@ -0,0 +1,46 @@
+// import 'dart:convert';
+//
+// import 'package:http/http.dart' as http;
+//
+// import '../Model/Weather_Model.dart';
+//
+// class WeatherServices{
+//   fetchWeather()async{
+//     final response = await http.get(Uri.parse("https://api.openweathermap.org/data/2.5/weather?lat=23.0225&lon=72.5714&appid=4c289369f9ebc8fe621c41b465ea8c16"));
+//     try{
+//       if(response.statusCode==200){
+//         var json = jsonDecode(response.body);
+//         return WeatherData.fromJson(json);
+//       } else{
+//         throw Exception('Failed to load Weather data');
+//       }
+//     }catch(e){
+//       print(e.toString());
+//     }
+//   }
+// }
+
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:weather_app/Model/weather_model.dart';
+
+class WeatherServices {
+  fetchWeather() async {
+    final response = await http.get(Uri.parse(
+        "https://api.openweathermap.org/data/2.5/weather?lat=23.0225&lon=72.5714&appid=4c289369f9ebc8fe621c41b465ea8c16"));
+    // now we can cange latitude and longitude and let's see how it perfrom.
+    try {
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+        return WeatherData.fromJson(json);
+      } else {
+        throw Exception('Failed to load Weather data');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
+// replace the api key with your api key thay openWeathemap provide you
+// for your current location provide the longitude and latitude of your current location
